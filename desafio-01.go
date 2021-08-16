539cd9c908a89c61da341c1110b88ae5a6cf273b
Francisco <novais181@gmail.com>
2021-08-16 19:10:20 -0300

    Desafio 01 - Feito

--- desafio-01.go
+++ desafio-01.go
@@ -1,4 +1,4 @@
-// Quero que você faã um função em golang de som e retorne o valor
+// Quero que você faça uma função em golang de some e retorne o valor
 // Vou colocar melhor
 // Quero que você faça um metodo que eu passe dois numeros  e operador(+ - * /) e retorne o valor
 // Casos
@@ -6,3 +6,38 @@
 // Novo
 // valo < 0 --> valor negativo, inválido
 
+package main
+
+import "fmt"
+
+func main() {
+	soma(5, 5, "+")
+	soma(5, 6, "-")
+	soma(5, 5, "/")
+	soma(5, 5, "*")
+	soma(5, 5, "+++")
+
+}
+
+func soma(num1 int, num2 int, operador string) {
+
+	if operador == "+" {
+		validaValor(num1 + num2)
+	} else if operador == "-" {
+		validaValor(num1 - num2)
+	} else if operador == "/" {
+		validaValor(num1 / num2)
+	} else if operador == "*" {
+		validaValor(num1 * num2)
+	} else {
+		fmt.Println("Operador inválido")
+	}
+}
+
+func validaValor(resultado int) {
+	if resultado < 0 {
+		fmt.Println("Valor negativo, inválido")
+	} else {
+		fmt.Println(resultado)
+	}
+}
