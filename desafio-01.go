7a2f793b6f30a2a841fcc972a9127b1de9e2d202
francisconvs <novais181@gmail.com>
2021-08-16 15:04:34 -0300

    Desafio 01

---
+++ desafio-01.go
@@ -0,0 +1,8 @@
+// Quero que você faã um função em golang de som e retorne o valor
+// Vou colocar melhor
+// Quero que você faça um metodo que eu passe dois numeros  e operador(+ - * /) e retorne o valor
+// Casos
+// valor >= 0 =--> retorne o valor da operação
+// Novo
+// valo < 0 --> valor negativo, inválido
+
