20fc87be8028fd24d8f18dd93d933faa1f725fd0
francisconvs <novais181@gmail.com>
2021-08-19 13:57:53 -0300

    Add segundo desafio

---
+++ Desafio-02/desafio-02.go
@@ -0,0 +1,12 @@
+// Quero que eu passe um parametro como por exemplo meu nome
+// tipo: ./pessoa jonas 
+// return feio
+// ./pessoa pedro
+// retun bonito
+// quero que quando receba o nome vc coloque este nome em u struct
+// e passe para o metodo
+// pessoa well
+// return lindao
+// pessoa chico
+// return tyomahowk
+
