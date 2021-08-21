3ab8538a9bfd930e696a09b279f4c73db3e29823
Francisco <novais181@gmail.com>
2021-08-21 17:26:23 -0300

    Desafio 02

--- Desafio-02/desafio-02.go
+++ Desafio-02/desafio-02.go
@@ -1,12 +1,46 @@
 // Quero que eu passe um parametro como por exemplo meu nome
-// tipo: ./pessoa jonas 
+// tipo: ./pessoa jonas
 // return feio
 // ./pessoa pedro
 // retun bonito
-// quero que quando receba o nome vc coloque este nome em u struct
+// quero que quando receba o nome vc coloque este nome em um struct
 // e passe para o metodo
 // pessoa well
 // return lindao
 // pessoa chico
 // return tyomahowk
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Digite um nome: ")
+	var nome string
+	fmt.Scanln(&nome)
+	fmt.Println(pessoa(nome))
+}
+
+func pessoa(nome string) string {
+
+	type individuo struct {
+		Name     string
+		Atributo string
+	}
+
+	meliante := individuo{nome, "vazio"}
+
+	if nome == "Pedro" {
+		meliante.Atributo = "Bonito"
+	} else if nome == "Jonas" {
+		meliante.Atributo = "Zica de mais"
+	} else if nome == "Well" {
+		meliante.Atributo = "Lindão"
+	} else if nome == "Chico" {
+		meliante.Atributo = "Bebe água"
+	}
+
+	return meliante.Atributo
+}
