644d9b7ef2ec5148710d0602404a8d8d80047219
francisconvs <novais181@gmail.com>
2021-09-10 09:08:12 -0300

    Add desafio 03

---
+++ Desafio-03/desafio-03.go
@@ -0,0 +1,11 @@
+// Existem N moedas, cada uma mostrando cara ou coroa. Gostaríamos que todas as moedas tivessem uma sequência de caras e coroas alternadas. Qual é o número mínimo de moedas que devem ser revertidas para conseguir isso?
+
+// Função de escrita:
+// 	def solution(A)
+
+// que, dado um array A consistindo de N inteiros representando as moedas, retorna o número mínimo de moedas que deve ser revertido. Os elementos consecutivos da matriz A representam moedas de censura e contêm 0 (cara) ou 1 (coroa).
+
+// Exemplos:
+
+// 1. Dado o array A = [1,0,1,0,1,1], a função deve retornar 1. Depois de inverter a sexta moeda, alcançamos uma sequência alternada de moedas [1,0,1,0,1, 0].
+
