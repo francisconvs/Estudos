f66599ebf891c44181afad377d2f172a82c01466
Francisco Novais <fnovais@pagseguro.com>
2023-12-15 15:19:50 -0300

    add playbook ansible

---
+++ Ansible/script.ps1
@@ -0,0 +1,29 @@
+# Install-WindowsFeature Web-Asp-Net45
+
+# Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+
+# Install-WindowsFeature Web-Server
+# Install-WindowsFeature Web-Default-Doc
+# Install-WindowsFeature Web-Dir-Browsing
+# Install-WindowsFeature Web-Http-Errors
+# Install-WindowsFeature Web-Static-Content
+# Install-WindowsFeature Web-Http-Redirect
+# Install-WindowsFeature Web-Http-Logging
+# Install-WindowsFeature Web-Custom-Logging
+# Install-WindowsFeature Web-Log-Libraries
+# Install-WindowsFeature Web-Request-Monitor
+# Install-WindowsFeature Web-Http-Tracing
+# Install-WindowsFeature Web-Stat-Compression
+# Install-WindowsFeature Web-Filtering
+# Install-WindowsFeature Web-Basic-Auth
+# Install-WindowsFeature Web-Digest-Auth
+# Install-WindowsFeature Web-IP-Security
+# Install-WindowsFeature Web-Url-Auth
+# Install-WindowsFeature Web-Windows-Auth
+# Install-WindowsFeature Web-Net-Ext45
+# Install-WindowsFeature Web-Asp-Net45
+# Install-WindowsFeature Web-WebSockets
+# Install-WindowsFeature Web-Mgmt-Console
+# Install-WindowsFeature Web-Scripting-Tools
+# Install-WindowsFeature Web-Mgmt-Service
